cc1e8509ab817976ea70d14b7492da583dbaf913
JulianaMancera <mancerajuliana98@gmail.com>
2026-02-04 13:33:51 +0800

    color and images

--- smartcart/lib/main.dart
+++ smartcart/lib/main.dart
@@ -5,10 +5,16 @@ void main() {
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Center(
-            child: Text("SmartCart"),
+          title: const Center(
+            child: Text("Pink Cart"),
             ),
+            backgroundColor: const Color.fromARGB(255, 203, 32, 95),
+          ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/guitar.jpg'),),
         ),
+        backgroundColor: Colors.pink[200],
       ),
     ),
   );
