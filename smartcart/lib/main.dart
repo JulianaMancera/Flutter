86117a407674c78b416732fbb1c18b7a8fd01217
JulianaMancera <mancerajuliana98@gmail.com>
2026-02-05 15:25:43 +0800

    padding

--- smartcart/lib/main.dart
+++ smartcart/lib/main.dart
@@ -1,21 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyWidget());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Center(
-            child: Text("Pink Cart"),
-            ),
-            backgroundColor: const Color.fromARGB(255, 203, 32, 95),
-          ),
-        body: const Center(
-          child: Image(
-            image: AssetImage('images/guitar.jpg'),),
+          title: const Center(child: Text("Pink Cart")),
+          backgroundColor: const Color.fromARGB(255, 225, 53, 96),
         ),
-        backgroundColor: Colors.pink[200],
+        body: Container(
+          color: const Color.fromARGB(255, 195, 92, 74),
+          width: 150.0,
+          height: 150.0,
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(20.0),
+          margin: EdgeInsets.all(150.0),
+          child: Text("Welcome to Pink Cart!"),
+        )
       ),
-    ),
-  );
+    );
+  }
 }
