dab0eeaf55627cb7083c4ea0553576b580d28813
JulianaMancera <mancerajuliana98@gmail.com>
2026-02-05 15:35:00 +0800

    draft

--- smartcart/lib/main.dart
+++ smartcart/lib/main.dart
@@ -11,18 +11,23 @@ class MyWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Center(child: Text("Pink Cart")),
-          backgroundColor: const Color.fromARGB(255, 225, 53, 96),
-        ),
-        body: Container(
-          color: const Color.fromARGB(255, 195, 92, 74),
-          width: 150.0,
-          height: 150.0,
-          alignment: Alignment.center,
-          padding: const EdgeInsets.all(20.0),
-          margin: EdgeInsets.all(150.0),
-          child: Text("Welcome to Pink Cart!"),
+        // appBar: AppBar(
+        //   title: const Center(child: Text("Pink Cart")),
+        //   backgroundColor: const Color.fromARGB(255, 225, 53, 96),
+        // ),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Container(
+              color: const Color.fromARGB(255, 195, 92, 74),
+              width: 150.0,
+              height: 150.0,
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(20.0),
+              //margin: EdgeInsets.all(150.0),
+              child: Text("Welcome to Pink Cart!"),
+              ),
+            ]) ,
         )
       ),
     );
